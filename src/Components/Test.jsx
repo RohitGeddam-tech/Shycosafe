ef2b0a796f3051d6de051c73118b867a1d8b6daf
rohitgeddam-tech <geddamrohi13@gmail.com>
2021-07-26 14:07:12 +0530

    first commit

--- src/Components/Test.jsx
+++ src/Components/Test.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import { useState } from "react";
-import banner from "../images/Oval.png";
+// import banner from "../images/Oval.png";
 import "./Test.scss";
 // import Modal from "../../Utils/Modal";
 import line from "../images/Rect2.png";
@@ -9,32 +9,33 @@ import CustomSlider from "./CustomSlider";
 const bannerDetails = [
   {
     id: 1,
-    image: banner,
+    word: 'PG',
     title: "Priyank Garg",
     info: "Joint MD - Advance Valves",
     para: "The pandemic has led to huge work disruptions. Our factories were shut since March last year and started operating partially from the first week of May, before regaining full operations as per government guidelines. The safety and wellbeing of all our employees is our foremost priority and concern. We decided to go for Shycocan as it is a state-of-the-art technology that is user-friendly, low on running costs and extremely effective indoors.",
   },
   {
     id: 2,
-    image: banner,
+    word: 'VK',
     title: "Vijay Kaul",
     info: "Colonel - Indian Army",
     para: "I wish to put on record the Excellent Customer Care and After Sales Service provided by your esteemed company. Post raising an issue in the Sycocan, very high standard advisory and assurance was given to us. Mr Gowda Raghu and Mr Malang stand out in solving the issue like thorough professionals. They deserve our special thanks and appreciation. ",
   },
   {
     id: 3,
-    image: banner,
+    word: 'PG',
     title: "Priyank Garg",
     info: "Joint MD - Advance Valves",
     para: "The pandemic has led to huge work disruptions. Our factories were shut since March last year and started operating partially from the first week of May, before regaining full operations as per government guidelines. The safety and wellbeing of all our employees is our foremost priority and concern. We decided to go for Shycocan as it is a state-of-the-art technology that is user-friendly, low on running costs and extremely effective indoors.",
   },
 ];
 
-const Banner1 = ({ image, title, para, info, id }) => {
+const Banner1 = ({ word, title, para, info, id }) => {
   return (
     <div className={`Banner${id}`}>
       <div className="head">
-        <img src={image} alt="banner" />
+        {/* <img src={image} alt="banner" /> */}
+        <h2>{word}</h2>
         <div className="data">
           <h1 className="heading">{title}</h1>
           <p>{info}</p>
