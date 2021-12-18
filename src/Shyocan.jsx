2c4acbcdfce2d1d7934474fc2db3aa43cb3ef825
rohitgeddam-tech <rohitgeddam0@gmail.com>
2021-12-18 14:59:53 +0530

    temp done

--- src/Shyocan.jsx
+++ src/Shyocan.jsx
@@ -1,12 +1,20 @@
 // import logo from './images/logo.png';
 import './App.scss';
-import About from './Components/About';
-import Collection from './Components/Collection';
-import Features from './Components/Features';
-import News from './Components/News';
-import Test from './Components/Test';
-import Footer from './layout/Footer';
-import NewHeader from './layout/NewHeader';
+import loadable from "@loadable/component";
+const About = loadable(() => import("./Components/About"));
+const Collection = loadable(() => import("./Components/Collection"));
+const Features = loadable(() => import("./Components/Features"));
+const News = loadable(() => import("./Components/News"));
+const Test = loadable(() => import("./Components/Test"));
+const Footer = loadable(() => import("./layout/Footer"));
+const NewHeader = loadable(() => import("./layout/NewHeader"));
+// import About from './Components/About';
+// import Collection from './Components/Collection';
+// import Features from './Components/Features';
+// import News from './Components/News';
+// import Test from './Components/Test';
+// import Footer from './layout/Footer';
+// import NewHeader from './layout/NewHeader';
 
 function Shycocan() {
   return (
